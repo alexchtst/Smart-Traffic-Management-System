780a4a0cc273476ba02f4c9c9a7975e0847ea3b1
alexchtst-01 <acinatra@gmail.com>
2025-02-28 19:18:45 +0700

    feat: add pull helper

--- pull-repo.sh
+++ pull-repo.sh
@@ -13,20 +13,17 @@ pullService(){
     echo "Pull dari branch $branch ..."
     git pull
 
-    cd - > /dev/null  # Kembali ke folder awal (opsional)
+    cd - > /dev/null
 }
 
-# Path lokal direktori masing-masing project
 FeDir="Smart-Traffic-Management-Frontend"
 BeDir="Smart-Traffic-Management-Backend"
 AiDir="Smart-Traffic-Management-AI"
 
-# Branch masing-masing project
 FeBranch="main"
 BeBranch="main"
 AiBranch="main"
 
-# Panggil fungsi
 pullService "$FeDir" "$FeBranch"
 pullService "$BeDir" "$BeBranch"
 pullService "$AiDir" "$AiBranch"
